b17e6a23833ddc6f7c41df5eab57b58068f5cd8e
Miguel Ángel Navas García <manavas@gmail.com>
2025-03-10 16:07:42 +0900

    first commit

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function MealPage() {
+  return (
+    <main>
+      Meal
+    </main>
+  )
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function SharePage() {
+  return (
+    <main>
+      Share
+    </main>
+  )
+}
