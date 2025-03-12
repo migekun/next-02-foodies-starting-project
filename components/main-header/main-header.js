249c6a5fee80316d5f373dd4db78c85cff337e46
Miguel Ángel Navas García <manavas@gmail.com>
2025-03-12 11:35:10 +0900

    Mucha mierda muy rapido

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,11 +1,16 @@
+'use client'
+
 import Link from "next/link";
 import Image from "next/image";
 
 import logo from "@/assets/logo.png";
 import classes from '@/components/main-header/main-header.module.css'
 import MainHeaderBackground from "@/components/main-header/main-header-background";
+import {usePathname} from "next/navigation";
+import NavLink from "@/components/main-header/nav-link";
 
-export default function mainHeader() {
+export default function MainHeader() {
+  const pathname = usePathname();
   return (<>
     <MainHeaderBackground/>
     <header className={classes.header}>
@@ -17,8 +22,12 @@ export default function mainHeader() {
       </Link>
       <nav className={classes.nav}>
         <ul>
-          <li><Link href="/meals">Browse Meals</Link></li>
-          <li><Link href="/community">Community</Link></li>
+          <li>
+            <NavLink href="/meals" >Browse Meals</NavLink>
+          </li>
+          <li>
+            <NavLink href="/community">Foodies Community</NavLink>
+           </li>
         </ul>
       </nav>
     </header>
