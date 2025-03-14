ecff336b9e6e6f1c873723f29f369bd96a599b84
Miguel Ángel Navas García <manavas@gmail.com>
2025-03-14 15:39:31 +0900

    Form submit to the server

---
+++ components/meals/image-picker.js
@@ -0,0 +1,48 @@
+'use client'
+
+import classes from "./image-picker.module.css";
+import { useRef, useState } from "react";
+import Image from "next/image";
+export default function ImagePicker({label, name}) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInput = useRef();
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
+  function handleImagePicked(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null)
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+  return <div className={classes.picker}>
+    <label htmlFor={name}>{label}</label>
+    <div className={classes.controls}>
+      <div className={classes.preview}>
+        {!pickedImage && <p>No image picked yet.</p>}
+        {pickedImage && <Image src={pickedImage} alt="The image selected by the user" fill/>}
+      </div>
+      <input
+        className={classes.input}
+        type="file"
+        id="image"
+        name={name}
+        accept="imagepng, image/jpeg, image/jpg"
+        ref={imageInput}
+        onChange={handleImagePicked}
+        required
+      />
+      <button className={classes.button} type="button" onClick={handlePickClick}>
+        Pick an Image
+      </button>
+    </div>
+  </div>
+}
