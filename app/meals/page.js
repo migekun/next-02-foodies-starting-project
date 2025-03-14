d562784617be7b27846e45266001b6b68de2300d
Miguel Ángel Navas García <manavas@gmail.com>
2025-03-14 10:16:20 +0900

    Anyadir pagina not found, error, loading que son predefinidas para next.js y se supone que te mejora la experiencia de programacion. Cada uno de estos ficheros se pueden crear en diferentes niveles, lo que no se es si se sobreescriben o el de nivel mayor tiene prioridad

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,14 @@
+import { Suspense } from 'react';
 import classes from "./page.module.css"
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
-import MealItem from "@/components/meals/meal-item";
 
-export default async function MealsPage() {
+async function Meals(){
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +21,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Feching meals...</p>}>
+          <Meals/>
+        </Suspense>
       </main>
     </>
   )
