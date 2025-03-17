766ce6a3325093e2bc5c8dee46ed15ee3aeb8566
Miguel Ángel Navas García <manavas@gmail.com>
2025-03-17 13:56:38 +0900

    Correcting the code

--- app/meals/meals-form-submit.js
+++ app/meals/meals-form-submit.js
@@ -1,9 +1,9 @@
 'use client'
 
-import { useFormState} from "react-dom";
+import { useFormStatus} from "react-dom";
 
 export default function MealsFormSubmit(){
-  const { pending } = useFormState();
+  const { pending } = useFormStatus();
   return (<button disabled={pending}>
     {pending ? 'Submitting...' : 'Share Meal'}
   </button>)
